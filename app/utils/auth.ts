7970010ed8d4795ea92de0abec8ac7f696976e1f
yashdev9274 <yashdev.yvd@gmail.com>
2025-02-11 01:05:46 +0530

    Add Prisma adapter for NextAuth and configure PostgreSQL datasource.

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import Github from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./db"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(prisma),
   providers: [Github],
 })
