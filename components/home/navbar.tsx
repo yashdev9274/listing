707d035566fb0df53910a36967bda3f8facf4152
yashdev9274 <yashdev.yvd@gmail.com>
2025-02-11 00:51:43 +0530

    Enhance Navbar component with authentication logic for login/logout functionality.

--- components/home/navbar.tsx
+++ components/home/navbar.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
-import { Button } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
 import { ModeToggle } from "./ThemeToggle";
+import { auth, signOut } from "@/app/utils/auth";
 
-export function Navbar(){
+export async function Navbar(){
+
+    const session = await auth()
     return(
         <nav className="flex justify-between items-center py-4">
             <Link href="/">
@@ -10,9 +13,21 @@ export function Navbar(){
             </Link>
             <div className="flex items-center space-x-4">
                 <ModeToggle />
-            <Button>
-                Login
-            </Button>
+                {session?.user ? (<form action = {async()=>{
+                    "use server"
+                    await signOut({
+                        redirectTo: "/",
+                    })
+                }}>
+                    <Button>
+                        Logout
+                    </Button>
+                </form>):(
+                    <Link href="/login" className={buttonVariants({variant: "outline"})}>
+                        Login
+                    </Link>
+                )}
+            
             </div>
         </nav>
     );
