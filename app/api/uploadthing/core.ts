7c2cd7274427cad1fa7161d950e229d490e952ca
yashdev9274 <yashdev.yvd@gmail.com>
2025-02-11 17:24:09 +0530

    Increase maximum file size limit to 4MB and add Next.js configuration for image domains.

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -13,11 +13,8 @@ export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({
     image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
-      maxFileSize: "2MB",
+      
+      maxFileSize: "4MB",
       maxFileCount: 1,
     },
   })
