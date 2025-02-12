c42c4604b69a142d333197e41cc5966456ece0c3
yashdev9274 <yashdev.yvd@gmail.com>
2025-02-12 15:48:59 +0530

    Add JobPost model and PostJob page with job creation form.

--- app/(appLayout)/postjob/page.tsx
+++ app/(appLayout)/postjob/page.tsx
@@ -1,5 +1,31 @@
-export default function PostJob(){
-    return (
-        <div>Post Jobs</div>
-    )
+'use client'
+
+// import requireUser from "@/app/utils/requireuser"
+import CreateJobForm from "@/components/form/createJobForm"
+import { Card, CardHeader, CardTitle } from "@/components/ui/card"
+
+export default function PostJob() {
+  return (
+    <section className="py-10">
+      <div className="container mx-auto px-4">
+        <div className="max-w-3xl mx-auto">
+          <Card>
+            <CardHeader>
+              <CardTitle>Post a New Job</CardTitle>
+            </CardHeader>
+            <div className="p-6">
+              <CreateJobForm 
+                companyAbout="Company name"
+                companyLocation="Company name"
+                companyLogo="Company name"
+                companyName="Company name"
+                companyXAccount="Company name"
+                companyWebsite="Company name"
+              />
+            </div>
+          </Card>
+        </div>
+      </div>
+    </section>
+  )
 }
---
+++ components/form/createJobForm.tsx
@@ -0,0 +1,421 @@
+'use client'
+
+import { z } from "zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { jobSchema } from "@/app/utils/zodschema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Input } from "../ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
+import { Textarea } from "../ui/textarea";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { XIcon } from "lucide-react";
+import { UploadDropzone } from "../home/uploadthingexported";
+import { toast } from "sonner";
+import { countryList } from "@/app/utils/countrylist";
+import { useState } from "react";
+
+interface CreateJobFormProps {
+    companyName: string;
+    companyLocation: string;
+    companyAbout: string;
+    companyLogo: string;
+    companyXAccount: string | null;
+    companyWebsite: string;
+}
+
+
+
+export default function CreateJobForm({
+  companyAbout,
+  companyLocation,
+  companyLogo,
+  companyXAccount,
+  companyName,
+  companyWebsite,
+}: CreateJobFormProps
+) {
+
+    const form = useForm<z.infer<typeof jobSchema>>({
+        resolver: zodResolver(jobSchema),
+        defaultValues: {
+          benefits: [],
+          companyDescription: companyAbout,
+          companyLocation: companyLocation,
+          companyName: companyName,
+          companyWebsite: companyWebsite,
+          companyXAccount: companyXAccount || "",
+          employmentType: "",
+          jobDescription: "",
+          jobTitle: "",
+          location: "",
+          salaryFrom: 0,
+          salaryTo: 0,
+          companyLogo: companyLogo,
+          listingDuration: 30,
+        },
+      });
+
+      const [pending, setPending] = useState(false);
+async function onSubmit(values: z.infer<typeof jobSchema>) {
+    try {
+      setPending(true);
+
+      // await createJob(values);
+    } catch {
+      toast.error("Something went wrong. Please try again.");
+    } finally {
+      setPending(false);
+    }
+}
+    
+    return(
+        <div>
+            <Form {...form}>
+            <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="col-span-1   lg:col-span-2  flex flex-col gap-8"
+      >
+        <Card>
+          <CardHeader>
+            <CardTitle>Job Information</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-6">
+            <div className="grid md:grid-cols-2 gap-6">
+              <FormField
+                control={form.control}
+                name="jobTitle"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Job Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Job Title" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="employmentType"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Employment Type</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select Employment Type" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Employment Type</SelectLabel>
+                          <SelectItem value="full-time">Full Time</SelectItem>
+                          <SelectItem value="part-time">Part Time</SelectItem>
+                          <SelectItem value="contract">Contract</SelectItem>
+                          <SelectItem value="internship">Internship</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="grid md:grid-cols-2 gap-6">
+              <FormField
+                control={form.control}
+                name="location"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Location</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select Location" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Worldwide</SelectLabel>
+                          <SelectItem value="worldwide">
+                            <span>🌍</span>
+                            <span className="pl-2">Worldwide / Remote</span>
+                          </SelectItem>
+                        </SelectGroup>
+                        <SelectGroup>
+                          <SelectLabel>Location</SelectLabel>
+                          {countryList.map((country) => (
+                            <SelectItem value={country.name} key={country.code}>
+                              <span>{country.flagEmoji}</span>
+                              <span className="pl-2">{country.name}</span>
+                            </SelectItem>
+                          ))}
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormItem>
+                <FormLabel>Salary Range</FormLabel>
+                <FormControl>
+                  {/* <SalaryRangeSelector
+                    control={form.control}
+                    minSalary={30000}
+                    maxSalary={1000000}
+                  /> */}
+                </FormControl>
+                <FormMessage>
+                  {form.formState.errors.salaryFrom?.message ||
+                    form.formState.errors.salaryTo?.message}
+                </FormMessage>
+              </FormItem>
+            </div>
+
+            <FormField
+              control={form.control}
+              name="jobDescription"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job Description</FormLabel>
+                  <FormControl>
+                    {/* <JobDescriptionEditor field={field} /> */}
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="benefits"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Benefits</FormLabel>
+                  <FormControl>
+                    {/* <BenefitsSelector field={field} /> */}
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Company Information</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-6">
+            <div className="grid md:grid-cols-2 gap-6">
+              <FormField
+                control={form.control}
+                name="companyName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Company Name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="companyLocation"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Location</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select Location" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Worldwide</SelectLabel>
+                          <SelectItem value="worldwide">
+                            <span>🌍</span>
+                            <span className="pl-2">Worldwide</span>
+                          </SelectItem>
+                        </SelectGroup>
+                        <SelectGroup>
+                          <SelectLabel>Location</SelectLabel>
+                          {countryList.map((country) => (
+                            <SelectItem value={country.name} key={country.name}>
+                              <span>{country.flagEmoji}</span>
+                              <span className="pl-2">{country.name}</span>
+                            </SelectItem>
+                          ))}
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="grid md:grid-cols-2 gap-6">
+              <FormField
+                control={form.control}
+                name="companyWebsite"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company Website</FormLabel>
+                    <FormControl>
+                      <div className="flex ">
+                        <span className="flex items-center justify-center px-3 border border-r-0 border-input rounded-l-md bg-muted text-muted-foreground text-sm">
+                          https://
+                        </span>
+                        <Input
+                          {...field}
+                          placeholder="Company Website"
+                          className="rounded-l-none"
+                        />
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="companyXAccount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Company X Account</FormLabel>
+                    <FormControl>
+                      <div className="flex ">
+                        <span className="flex items-center justify-center px-3 border border-r-0 border-input rounded-l-md bg-muted text-muted-foreground text-sm">
+                          @
+                        </span>
+                        <Input
+                          {...field}
+                          placeholder="Company X Account"
+                          className="rounded-l-none"
+                        />
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <FormField
+              control={form.control}
+              name="companyDescription"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Company Description"
+                      className="min-h-[120px]"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="companyLogo"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company Logo</FormLabel>
+                  <FormControl>
+                    <div>
+                      {field.value ? (
+                        <div className="relative w-fit">
+                          {/* <Image
+                            src={field.value}
+                            alt="Company Logo"
+                            width={100}
+                            height={100}
+                            className="rounded-lg"
+                          /> */}
+                          <Button
+                            type="button"
+                            variant="destructive"
+                            size="icon"
+                            className="absolute -top-2 -right-2 "
+                            onClick={() => field.onChange("")}
+                          >
+                            <XIcon className="h-4 w-4" />
+                          </Button>
+                        </div>
+                      ) : (
+                        <UploadDropzone
+                          endpoint="imageUploader"
+                          onClientUploadComplete={(res) => {
+                            field.onChange(res[0].url);
+                            toast.success("Logo uploaded successfully!");
+                          }}
+                          onUploadError={() => {
+                            toast.error(
+                              "Something went wrong. Please try again."
+                            );
+                          }}
+                        />
+                      )}
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Job Listing Duration</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <FormField
+              control={form.control}
+              name="listingDuration"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    {/* <JobListingDurationSelector field={field} /> */}
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+        </Card>
+        {/* <Button type="submit" className="w-full" disabled={pending}>
+          {pending ? "Submitting..." : "Continue"}
+        </Button> */}
+      </form>
+            </Form>
+        </div>
+    )
+}
