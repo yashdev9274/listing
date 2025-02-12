a4b15da2f7a10b9920d432e6d4a623ba42a56bff
yashdev9274 <yashdev.yvd@gmail.com>
2025-02-12 22:48:07 +0530

    Fetch and display active job listings in JobListing component; enhance UI with Card layout and handle empty state.

--- components/home/jobListing.tsx
+++ components/home/jobListing.tsx
@@ -1,10 +1,60 @@
-"use client"
+
+import { prisma } from "@/app/utils/db"
+import { Card, CardHeader } from "../ui/card"
+
+async function getData(){
+    const data = await prisma.jobPost.findMany({
+        where:{
+            status: "ACTIVE",
+        },
+        select:{
+            jobTitle: true,
+            id: true,
+            salaryFrom: true,
+            salaryTo: true,
+            employmentType: true,
+            location: true,
+            createdAt: true,
+            company: {
+              select: {
+                name: true,
+                logo: true,
+                location: true,
+                about: true,
+              },
+            },
+        },
+        orderBy: {
+            createdAt: "desc",
+          },
+        
+    });
+    return data;
+    
+}
+
+export default async function JobListing() {
+    const data = await getData();
 
 
-export default function JobListing() {
+
     return(
-        <h1>
-            Job Listing
-        </h1>
+        <Card className="space-y-4">
+            <CardHeader className="space-y-4">
+                <div className="flex items-center justify-between">
+
+                    {data.length>0?(
+                        <div className="space-y-4">
+                            {data.map((job)=>(
+                                <p key = {job.id}>{job.jobTitle}</p>
+                            ))}
+                        </div>
+                    ):(
+                        <p>No jobs found</p>
+                    )}
+                </div>
+            </CardHeader>
+
+        </Card>
     )
 }
